16e61e510678a3daba24c77ed94a5b71f2d15a33
Robin Avery <robin.one.half@gmail.com>
2018-08-16 13:52:45 -0400

    Begin working on Introduction to Parsers

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
-export function sampleFunction(x: string): string {
-  return x + x;
+
+const char = (c: string) => (input: string) => {
+  if (input[0] === c) {
+    return { success: true, rest: input.slice(1) }
+  }
+  return { success: false, rest: input }
 }
+console.log(char('a')('abc'));
+// => { success: true, rest: 'bc' }
