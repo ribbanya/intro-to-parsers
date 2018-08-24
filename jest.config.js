ef20fbecb434923aa6b6b95143f81329d6ea642a
Robin Avery <ribbanya@gmail.com>
2018-08-24 03:14:55 -0400

    Replace quotemarks in jest config

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,11 @@
 module.exports = {
   transform: {
-    "^.+\\.tsx?$": "ts-jest",
+    '^.+\\.tsx?$': 'ts-jest',
   },
-  testRegex: "(?:/tests/.*|/src/.*?(?:\\.|/)(?:test|spec)\\.(?:jsx?|tsx?))$",
-  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+  testRegex: '(?:/tests/.*|/src/.*?(?:\\.|/)(?:test|spec)\\.(?:jsx?|tsx?))$',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   collectCoverage: true,
   collectCoverageFrom: [
-    "<rootDir>/src/**/*.{js,jsx,ts,tsx}",
+    '<rootDir>/src/**/*.{js,jsx,ts,tsx}',
   ],
 };
